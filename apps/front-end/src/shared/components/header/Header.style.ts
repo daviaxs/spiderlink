1fb578b186fab96535fcb5650a0635c60f0e3c3e
Davi Alves <cadwvk@gmail.com>
2024-01-27 00:42:24 -0300

    feat: Update header style and add header info

--- apps/front-end/src/shared/components/header/Header.style.ts
+++ apps/front-end/src/shared/components/header/Header.style.ts
@@ -13,5 +13,16 @@ export const HeaderStyle = styled.header`
   padding: 1.25rem 0.625rem;
 
   background-color: ${themeColors['dark-blue-50']};
-  border-bottom: 1px solid ${themeColors['dark-blue-200']};
+  border-bottom: 1px solid ${themeColors['dark-blue-100']};
+
+  .header-info {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.9375rem;
+
+    h2 {
+      margin-bottom: 0.3rem;
+    }
+  }
 `
--- apps/front-end/src/shared/components/header/Header.tsx
+++ apps/front-end/src/shared/components/header/Header.tsx
@@ -1,5 +1,26 @@
+import { MenuIcon } from '@/shared/assets/svgs'
 import { HeaderStyle } from './Header.style'
+import { IconButton } from '../buttons/IconButton'
+import { Text } from '../text/Text'
 
 export function Header() {
-  return <HeaderStyle>Header</HeaderStyle>
+  return (
+    <HeaderStyle>
+      <div className="header-info">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+
+        <Text
+          as="h2"
+          size={30}
+          color="dark-blue-800"
+          fontVariant="all-small-caps"
+          $weight="900"
+        >
+          spiderlink
+        </Text>
+      </div>
+    </HeaderStyle>
+  )
 }
