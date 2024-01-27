78fe029188635277ad549c828f60e9f1b0e1e5e7
Davi Alves <cadwvk@gmail.com>
2024-01-27 00:31:16 -0300

    feat: Add Text component to shared components

---
+++ apps/front-end/src/shared/components/text/Text.ts
@@ -0,0 +1,25 @@
+'use client'
+
+import { themeColors } from '@/shared/style/theme/pallete'
+import styled from 'styled-components'
+
+interface TextProps {
+  as: keyof JSX.IntrinsicElements
+  color?: keyof typeof themeColors
+  size: number
+  $weight?: '200' | '300' | '400' | '500' | '700' | '800' | '900'
+  $textalign?: 'center' | 'left' | 'right'
+  $lineheight?: string
+  $whiteSpace?: 'normal' | 'nowrap'
+}
+
+export const Text = styled.p<TextProps>`
+  color: ${({ color }) =>
+    themeColors[color as keyof typeof themeColors] ||
+    themeColors['dark-blue-800']};
+  font-size: ${({ size }) => size}px;
+  font-weight: ${({ $weight }) => $weight};
+  line-height: ${({ $lineheight }) => $lineheight || '100%'};
+  text-align: ${({ $textalign }) => $textalign};
+  white-space: ${({ $whiteSpace }) => $whiteSpace || 'normal'};
+`
