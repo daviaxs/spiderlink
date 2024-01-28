774bf25984ab7522911cd8ec50d32986710eb7f0
Davi Alves <cadwvk@gmail.com>
2024-01-28 18:33:57 -0300

    feat: Update theme colors in theme.ts

--- apps/front-end/src/shared/style/theme/theme.ts
+++ apps/front-end/src/shared/style/theme/theme.ts
@@ -1,23 +1,31 @@
 import { themeColors } from './pallete'
 
 export const lightTheme = {
+  current: 'claro',
+
   body: themeColors.white,
   bodySecondary: themeColors['dark-blue-50'],
 
   border: themeColors['dark-blue-100'],
 
   title: themeColors['dark-blue-800'],
+  subtext: themeColors['dark-blue-400'],
 
   icon: themeColors['dark-blue-800'],
+  iconContainer: themeColors['dark-blue-100'],
 }
 
 export const darkTheme = {
+  current: 'escuro',
+
   body: themeColors['dark-blue-900'],
   bodySecondary: themeColors['dark-blue-800'],
 
   border: themeColors['dark-blue-500'],
 
   title: themeColors.white,
+  subtext: themeColors['dark-blue-200'],
 
   icon: themeColors['dark-blue-50'],
+  iconContainer: themeColors['dark-blue-500'],
 }
