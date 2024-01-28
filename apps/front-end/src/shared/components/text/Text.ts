a1df2dceb98a7535b5fbe5b04850df9777867a73
Davi Alves <cadwvk@gmail.com>
2024-01-28 18:09:48 -0300

    feat: Remove unused code and refactor Text component

--- apps/front-end/src/shared/components/text/Text.ts
+++ apps/front-end/src/shared/components/text/Text.ts
@@ -1,11 +1,9 @@
 'use client'
 
-import { themeColors } from '@/shared/style/theme/pallete'
 import styled from 'styled-components'
 
 interface TextProps {
   as: keyof JSX.IntrinsicElements
-  color?: keyof typeof themeColors
   size: number
   $weight?: '200' | '300' | '400' | '500' | '700' | '800' | '900'
   $textalign?: 'center' | 'left' | 'right'
@@ -21,9 +19,6 @@ interface TextProps {
 }
 
 export const Text = styled.p<TextProps>`
-  color: ${({ color }) =>
-    themeColors[color as keyof typeof themeColors] ||
-    themeColors['dark-blue-800']};
   font-size: ${({ size }) => size}px;
   font-weight: ${({ $weight }) => $weight};
   line-height: ${({ $lineheight }) => $lineheight || '100%'};
