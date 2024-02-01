72900e0a481bfe015fe66e7616a8989080e52019
Davi Alves <cadwvk@gmail.com>
2024-02-01 15:36:16 -0300

    feat: Add Prisma client configuration

---
+++ apps/back-end/src/lib/prisma.ts
@@ -0,0 +1,21 @@
+import { env } from '@/env'
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient
+}
+
+let prisma: PrismaClient
+
+if (env.NODE_ENV === 'prod') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient()
+  }
+
+  prisma = global.cachedPrisma
+}
+
+export const prismaClient = prisma
