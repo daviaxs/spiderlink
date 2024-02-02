462c1d403b0ffbc7a5fffc8da5e7323cb89ac02f
Davi Alves <cadwvk@gmail.com>
2024-02-02 14:30:49 -0300

    feat: Add fastifyJwt for JWT authentication

--- apps/back-end/src/app.ts
+++ apps/back-end/src/app.ts
@@ -1,5 +1,6 @@
 import fastifyCookie from '@fastify/cookie'
 import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { env } from './env'
 
@@ -8,6 +9,15 @@ export const app = fastify()
 app.register(fastifyCors, {
   origin: env.FRONT_END_URL,
   credentials: true,
+})
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: { expiresIn: '1h' },
 })
 
 app.register(fastifyCookie)
