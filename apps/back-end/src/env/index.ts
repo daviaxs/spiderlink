4ef6809e9679bde270fd980866bb3237f57ac40e
Davi Alves <cadwvk@gmail.com>
2024-02-02 14:31:01 -0300

    feat: Update environment variables validation

--- apps/back-end/src/env/index.ts
+++ apps/back-end/src/env/index.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config'
-import { z } from "zod";
+import { z } from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["dev", "prod"]),
+  NODE_ENV: z.enum(['dev', 'prod']),
+  JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
   FRONT_END_URL: z.string(),
 })
