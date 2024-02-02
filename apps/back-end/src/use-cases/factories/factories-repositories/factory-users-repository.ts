678119bf598c256dc989706d1aea22f27d72ee32
Davi Alves <cadwvk@gmail.com>
2024-02-02 14:44:57 -0300

    ref: Refactor factoryUsersRepository instantiation

--- apps/back-end/src/use-cases/factories/factories-repositories/factory-users-repository.ts
+++ apps/back-end/src/use-cases/factories/factories-repositories/factory-users-repository.ts
@@ -1,8 +1,3 @@
-import { env } from '@/env'
-import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 
-export const factoryUsersRepository =
-  env.NODE_ENV === 'prod'
-    ? new PrismaUsersRepository()
-    : new InMemoryUsersRepository()
+export const factoryUsersRepository = new PrismaUsersRepository()
