c7e8cf0af01bb94c9b972842d9be22b2ddb64e31
Davi Alves <cadwvk@gmail.com>
2024-02-02 15:20:15 -0300

    feat: Add domain connection to categories in PrismaCategoriesRepository

--- apps/back-end/src/repositories/categories-repository.ts
+++ apps/back-end/src/repositories/categories-repository.ts
@@ -1,7 +1,10 @@
 import { Categories, Prisma } from '@prisma/client'
 
 export interface CategoriesRepository {
-  addCategory(category: Prisma.CategoriesCreateInput): Promise<Categories>
+  addCategory(
+    category: Prisma.CategoriesCreateInput,
+    domainName: string,
+  ): Promise<Categories>
   deleteCategory(id: string): Promise<void>
   listCategories(domainName: string): Promise<Categories[]>
 }
--- apps/back-end/src/repositories/prisma/prisma-categories-repository.ts
+++ apps/back-end/src/repositories/prisma/prisma-categories-repository.ts
@@ -1,11 +1,32 @@
 import { Prisma } from '@prisma/client'
 import { CategoriesRepository } from '../categories-repository'
 import { prismaClient } from '@/lib/prisma'
+import { DomainWhereUniqueInput } from '@/@types/prisma'
 
 export class PrismaCategoriesRepository implements CategoriesRepository {
-  async addCategory(category: Prisma.CategoriesCreateInput) {
+  async addCategory(
+    category: Prisma.CategoriesCreateInput,
+    domainName: string,
+  ) {
+    const domain = await prismaClient.domain.findUnique({
+      where: {
+        name: domainName,
+      } as DomainWhereUniqueInput,
+    })
+
+    if (!domain) {
+      throw new Error(`Domain [${domainName}] not found`)
+    }
+
     const newCategory = await prismaClient.categories.create({
-      data: category,
+      data: {
+        ...category,
+        Domain: {
+          connect: {
+            id: domain.id,
+          },
+        },
+      },
     })
 
     return newCategory
