b360844b86132b3e94b4711d47786d611e9f76e9
Davi Alves <cadwvk@gmail.com>
2024-02-04 16:43:04 -0300

    Add factoryDomainsRepository to makeAuthenticateUseCase

--- apps/back-end/src/use-cases/factories/users/make-authenticate-use-case.ts
+++ apps/back-end/src/use-cases/factories/users/make-authenticate-use-case.ts
@@ -1,8 +1,12 @@
 import { AuthenticateUseCase } from '../../users/authenticate'
+import { factoryDomainsRepository } from '../factories-repositories/factory-domains-repository'
 import { factoryUsersRepository } from '../factories-repositories/factory-users-repository'
 
 export function makeAuthenticateUseCase() {
-  const authenticateUseCase = new AuthenticateUseCase(factoryUsersRepository)
+  const authenticateUseCase = new AuthenticateUseCase(
+    factoryUsersRepository,
+    factoryDomainsRepository,
+  )
 
   return authenticateUseCase
 }
