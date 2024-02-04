20bd2e9f091eba2f952a92d7513d7d9b1246eec8
Davi Alves <cadwvk@gmail.com>
2024-02-04 15:39:56 -0300

    feat: Add deleteDomain controller and route

---
+++ apps/back-end/src/http/controllers/domains/deleteDomain.ts
@@ -0,0 +1,27 @@
+import { makeDeleteDomainUseCase } from '@/use-cases/factories/domains/make-delete-domain-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function deleteDomain(req: FastifyRequest, reply: FastifyReply) {
+  const deleteDomainBodySchema = z.object({
+    domainName: z.string(),
+  })
+
+  const parsed = deleteDomainBodySchema.safeParse(req.body)
+
+  if (!parsed.success) {
+    const message = parsed.error.errors[0].message
+    return reply.status(400).send({ message })
+  }
+
+  const { domainName } = parsed.data
+
+  try {
+    const deleteDomainUseCase = makeDeleteDomainUseCase()
+
+    await deleteDomainUseCase.execute(domainName)
+  } catch (err) {
+    const error = err as Error
+    return reply.status(400).send({ message: error.message })
+  }
+}
--- apps/back-end/src/http/controllers/domains/routes.ts
+++ apps/back-end/src/http/controllers/domains/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { createDomain } from './createDomain'
+import { deleteDomain } from './deleteDomain'
+import { VerifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function domainsRoutes(app: FastifyInstance) {
   app.post('/domains', createDomain)
+  app.delete('/domains', { onRequest: [VerifyJWT] }, deleteDomain)
 }
