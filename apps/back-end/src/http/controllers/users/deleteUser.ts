f9770f383786810fb7a9449708820bd0deb945dd
Davi Alves <cadwvk@gmail.com>
2024-02-05 17:58:39 -0300

    feat: Add deleteUser controller and route

---
+++ apps/back-end/src/http/controllers/users/deleteUser.ts
@@ -0,0 +1,27 @@
+import { makeDeleteUserUseCase } from '@/use-cases/factories/users/make-delete-user-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function deleteUser(req: FastifyRequest, reply: FastifyReply) {
+  const deleteUserBodySchema = z.object({
+    userId: z.string(),
+  })
+
+  const parsed = deleteUserBodySchema.safeParse(req.params)
+
+  if (!parsed.success) {
+    const message = parsed.error.errors[0].message
+    return reply.status(400).send({ message })
+  }
+
+  const { userId } = parsed.data
+
+  try {
+    const deleteUserUseCase = makeDeleteUserUseCase()
+
+    await deleteUserUseCase.execute({ userId })
+  } catch (err) {
+    const error = err as Error
+    return reply.status(400).send({ message: error.message })
+  }
+}
--- apps/back-end/src/http/controllers/users/routes.ts
+++ apps/back-end/src/http/controllers/users/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { createAccount } from './createAccount'
 import { authenticate } from './authenticate'
+import { deleteUser } from './deleteUser'
+import { VerifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', createAccount)
   app.post('/users/authenticate', authenticate)
+  app.delete('/users/:userId', { onRequest: [VerifyJWT] }, deleteUser)
 }
