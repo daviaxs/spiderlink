020591c50a6747d4a9ec9b47d22231f9b6d05bb8
Davi Alves <cadwvk@gmail.com>
2024-02-05 18:48:58 -0300

    feat: Add getUserProfile controller and route

--- apps/back-end/src/http/controllers/users/routes.ts
+++ apps/back-end/src/http/controllers/users/routes.ts
@@ -3,9 +3,11 @@ import { createAccount } from './createAccount'
 import { authenticate } from './authenticate'
 import { deleteUser } from './deleteUser'
 import { VerifyJWT } from '@/http/middlewares/verify-jwt'
+import { getUserProfile } from './getUserProfile'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', createAccount)
   app.post('/users/authenticate', authenticate)
+  app.get('/users/profile', { onRequest: [VerifyJWT] }, getUserProfile)
   app.delete('/users/:userId', { onRequest: [VerifyJWT] }, deleteUser)
 }
