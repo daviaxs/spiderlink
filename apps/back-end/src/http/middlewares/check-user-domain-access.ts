0559c6a2c54ffd28c6be4f190ee26d8e580162b7
Davi Alves <cadwvk@gmail.com>
2024-02-06 17:47:31 -0300

    feat: Add CheckUserDomainAccess middleware

---
+++ apps/back-end/src/http/middlewares/check-user-domain-access.ts
@@ -0,0 +1,33 @@
+import { User } from '@prisma/client'
+import { FastifyReply, FastifyRequest, RouteGenericInterface } from 'fastify'
+
+interface RouteParams {
+  id: string
+}
+
+interface MyRouteGenericInterface extends RouteGenericInterface {
+  Params: RouteParams
+}
+
+export async function CheckUserDomainAccess(
+  req: FastifyRequest<MyRouteGenericInterface>,
+  reply: FastifyReply,
+) {
+  try {
+    const user: User = await req.jwtVerify()
+
+    if (!user) {
+      return reply.status(403).send({ message: 'Access denied' })
+    }
+
+    if (user.role === 'ADMIN') {
+      const domainId = req.params.id
+
+      if (domainId !== user.domainId) {
+        return reply.status(403).send({ message: 'Access denied' })
+      }
+    }
+  } catch (err) {
+    return reply.status(401).send({ message: 'Unauthorized' })
+  }
+}
