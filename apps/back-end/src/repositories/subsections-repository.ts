abbc61f4cadb80ee722a867bc0ab5e25ea84a160
Davi Alves <cadwvk@gmail.com>
2024-02-09 17:07:49 -0300

    ref: Update SubsectionsRepository interface to use Prisma Subsection type

--- apps/back-end/src/repositories/subsections-repository.ts
+++ apps/back-end/src/repositories/subsections-repository.ts
@@ -1,13 +1,21 @@
-import { Prisma, Subsections } from '@prisma/client'
+import { Prisma, Subsection } from '@prisma/client'
 
 export interface SubsectionsRepository {
   addSubsection(
-    subSection: Prisma.SubsectionsCreateInput,
-  ): Promise<Subsections | null>
+    subSection: Prisma.SubsectionCreateInput,
+    productId: string,
+    domainId: string,
+  ): Promise<Subsection | null>
 
   updateSubsection(
-    subSection: Prisma.SubsectionsUpdateInput,
-  ): Promise<Subsections | null>
+    subSection: Prisma.SubsectionUpdateInput,
+    productId: string,
+    domainId: string,
+  ): Promise<Subsection | null>
 
   deleteSubsection(id: string): Promise<void>
+
+  listSubsections(productId: string, domainId: string): Promise<Subsection[]>
+
+  findSubsectionById(subsectionId: string): Promise<Subsection | null>
 }
