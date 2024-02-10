73eb2b7fa990d9fac002129f74a79205c33cc01f
Davi Alves <cadwvk@gmail.com>
2024-02-09 22:35:40 -0300

    feat: Add product validation in ListSubsectionsUseCase

--- apps/back-end/src/use-cases/subsections/list-subsections.ts
+++ apps/back-end/src/use-cases/subsections/list-subsections.ts
@@ -1,9 +1,20 @@
+import { ProductsRepository } from '@/repositories/products-repository'
 import { SubsectionsRepository } from '@/repositories/subsections-repository'
+import { ProductNotFoundError } from '../errors/product-not-found-error'
 
 export class ListSubsectionsUseCase {
-  constructor(private subsectionsRepository: SubsectionsRepository) {}
+  constructor(
+    private subsectionsRepository: SubsectionsRepository,
+    private productsRepository: ProductsRepository,
+  ) {}
 
   async execute(productId: string, domainId: string) {
+    const product = await this.productsRepository.findProductById(productId)
+
+    if (!product) {
+      throw new ProductNotFoundError()
+    }
+
     const subsections = await this.subsectionsRepository.listSubsections(
       productId,
       domainId,
