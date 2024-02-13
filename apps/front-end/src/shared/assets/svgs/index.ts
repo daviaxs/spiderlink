c7d00996c7af829fe9de769a54051e7e7a5224e8
Davi Alves <cadwvk@gmail.com>
2024-02-13 17:33:13 -0300

    Add Status SVG component

--- apps/front-end/src/shared/assets/svgs/index.ts
+++ apps/front-end/src/shared/assets/svgs/index.ts
@@ -1,8 +1,9 @@
 import { MenuIcon } from './Menu'
 import { Motorcycle } from './Motorcycle'
+import { Status } from './Status'
 
 export interface SvgsProps {
   color?: string
 }
 
-export { MenuIcon, Motorcycle }
+export { MenuIcon, Motorcycle, Status }
