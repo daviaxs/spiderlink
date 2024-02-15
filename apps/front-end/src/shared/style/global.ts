567bc6e82c25b0a6f791bacce0507c5b85d7dd28
Davi Alves <cadwvk@gmail.com>
2024-02-15 15:10:41 -0300

    css: Add autofill styling to input fields

--- apps/front-end/src/shared/style/global.ts
+++ apps/front-end/src/shared/style/global.ts
@@ -13,6 +13,16 @@ export const GlobalStyle = createGlobalStyle`
     transition: background-color 200ms, color 100ms;
   }
 
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover, 
+  input:-webkit-autofill:focus, 
+  input:-webkit-autofill:active{
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: ${(props) => props.theme.title};
+    transition: background-color 5000s ease-in-out 0s;
+    box-shadow: inset 0 0 20px 20px transparent;
+  }
+
   a {
     text-decoration: none;
     cursor: pointer;
