17527b73c501c57aa03361826f87f587e4ff889c
Davi Alves <cadwvk@gmail.com>
2024-02-15 20:56:53 -0300

    feat: Add middleware for authentication in front-end app

---
+++ apps/front-end/src/middleware.ts
@@ -0,0 +1,20 @@
+import { SPIDER_LINK_ACCESS_TOKEN } from '@/shared/constants/cookiesNames'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  console.log('middleware')
+
+  const token = req.cookies.get(SPIDER_LINK_ACCESS_TOKEN)?.value
+
+  const HomePageURL = new URL('/', req.url)
+
+  if (!token && req.nextUrl.pathname === '/admin') {
+    return NextResponse.redirect(HomePageURL)
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/admin/:path*'],
+}
