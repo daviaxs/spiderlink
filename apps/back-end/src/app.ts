a2b68d0014d542b2b208a2d4320bf99e05d94872
Davi Alves <cadwvk@gmail.com>
2024-02-16 13:52:16 -0300

    feat: Add rate limiting middleware to the backend

--- apps/back-end/src/app.ts
+++ apps/back-end/src/app.ts
@@ -2,6 +2,7 @@ import fastifyCookie from '@fastify/cookie'
 import fastifyCors from '@fastify/cors'
 import fastifyJwt from '@fastify/jwt'
 import multipart from '@fastify/multipart'
+import rateLimit from '@fastify/rate-limit'
 import fastify from 'fastify'
 import { v2 as cloudinary } from 'cloudinary'
 import { env } from './env'
@@ -33,6 +34,11 @@ cloudinary.config({
   cloud_name: env.CLOUDINARY_CLOUD_NAME,
   api_key: env.CLOUDINARY_API_KEY,
   api_secret: env.CLOUDINARY_SECRET_KEY,
+})
+
+app.register(rateLimit, {
+  max: 30,
+  timeWindow: '1 minute',
 })
 
 app.register(fastifyJwt, {
