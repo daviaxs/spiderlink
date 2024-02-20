b1cc55b1c28cae7adebde8be14abcd249f2dd90d
Davi Alves <cadwvk@gmail.com>
2024-02-20 18:20:06 -0300

    css: Update SchedulesRoot styling in Schedules.style.ts

--- apps/front-end/src/app/(app)/admin/perfil/horarios/Schedules.style.ts
+++ apps/front-end/src/app/(app)/admin/perfil/horarios/Schedules.style.ts
@@ -2,7 +2,7 @@
 
 import styled from 'styled-components'
 
-export const SchedulesRoot = styled.div`
+export const SchedulesRoot = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
