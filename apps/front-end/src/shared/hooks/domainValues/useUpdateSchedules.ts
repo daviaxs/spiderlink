c6e2a767b9506a26d35e255e16d67e807f4ab609
Davi Alves <cadwvk@gmail.com>
2024-02-20 17:30:32 -0300

    feat: Add useUpdateSchedules hook for updating schedules

---
+++ apps/front-end/src/shared/hooks/domainValues/useUpdateSchedules.ts
@@ -0,0 +1,54 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { api } from '@/lib/axios'
+import { useState } from 'react'
+import { userAccesToken } from '../../constants/cookiesValues'
+import {
+  scheduleSchemaData,
+  scheduleSchema,
+} from '../../schemas/SchedulesSchema'
+
+export function useUpdateSchedules() {
+  const [errorMessage, setErrorMessage] = useState<string | null>()
+  const [successMessage, setSuccessMessage] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const methods = useForm<scheduleSchemaData>({
+    resolver: zodResolver(scheduleSchema),
+    mode: 'onChange',
+  })
+
+  const updateSchedules = (data: scheduleSchemaData) => {
+    setLoading(true)
+
+    api
+      .patch(
+        `schedules/${process.env.NEXT_PUBLIC_DOMAIN_ID}/e14b0281-5fe0-445e-8e9b-29dedf042aa9`,
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${userAccesToken}`,
+          },
+        },
+      )
+      .then(() => {
+        setErrorMessage(null)
+        setSuccessMessage(true)
+        window.location.href = '/admin/perfil'
+      })
+      .catch((e) => {
+        setErrorMessage(e.response.data.message)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+  return {
+    loading,
+    successMessage,
+    errorMessage,
+    updateSchedules,
+    methods,
+  }
+}
