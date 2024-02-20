d5194ab4762d0e9158fb89268e8e2086e7852a17
Davi Alves <cadwvk@gmail.com>
2024-02-20 17:22:17 -0300

    Refactor: SchedulesDays component to use dynamic rendering

--- apps/front-end/src/shared/components/schedules/SchedulesDays.tsx
+++ apps/front-end/src/shared/components/schedules/SchedulesDays.tsx
@@ -6,144 +6,46 @@ import {
   Separator,
 } from './Schedules.style'
 import { inter } from '@/shared/style/theme/fonts'
-import { useContext } from 'react'
+import { Fragment, useContext } from 'react'
 import { SchedulesContext } from '@/shared/contexts/Schedules'
 
 export function SchedulesDays() {
   const { seg, ter, qua, qui, sex, sab, dom } = useContext(SchedulesContext)
 
+  const days = [
+    { name: 'seg', schedule: seg },
+    { name: 'ter', schedule: ter },
+    { name: 'qua', schedule: qua },
+    { name: 'qui', schedule: qui },
+    { name: 'sex', schedule: sex },
+    { name: 'sab', schedule: sab },
+    { name: 'dom', schedule: dom },
+  ]
+
   return (
     <SchedulesDaysComponent>
-      <SchedulesDay>
-        <Text
-          as="h3"
-          size={16}
-          className={inter.className}
-          fontVariant="small-caps"
-        >
-          SEG
-        </Text>
+      {days.map((day) => (
+        <Fragment key={day.name}>
+          <SchedulesDay>
+            <Text
+              as="h3"
+              size={24}
+              className={inter.className}
+              fontVariant="small-caps"
+            >
+              {day.name}
+            </Text>
 
-        <Text as="h3" size={16} $weight="500" className={inter.className}>
-          {seg.fechado
-            ? 'FECHADO'
-            : `${formatHour(seg.inicio)} - ${formatHour(seg.termino)}`}
-        </Text>
-      </SchedulesDay>
+            <Text size={18} $weight="500" className={inter.className}>
+              {day.schedule.fechado
+                ? 'FECHADO'
+                : `${formatHour(day.schedule.inicio)} - ${formatHour(day.schedule.termino)}`}
+            </Text>
+          </SchedulesDay>
 
-      <Separator />
-
-      <SchedulesDay>
-        <Text
-          as="h3"
-          size={16}
-          className={inter.className}
-          fontVariant="small-caps"
-        >
-          TER
-        </Text>
-
-        <Text as="h3" size={16} $weight="500" className={inter.className}>
-          {ter.fechado
-            ? 'FECHADO'
-            : `${formatHour(ter.inicio)} - ${formatHour(ter.termino)}`}
-        </Text>
-      </SchedulesDay>
-
-      <Separator />
-
-      <SchedulesDay>
-        <Text
-          as="h3"
-          size={16}
-          className={inter.className}
-          fontVariant="small-caps"
-        >
-          QUA
-        </Text>
-
-        <Text as="h3" size={16} $weight="500" className={inter.className}>
-          {qua.fechado
-            ? 'FECHADO'
-            : `${formatHour(qua.inicio)} - ${formatHour(qua.termino)}`}
-        </Text>
-      </SchedulesDay>
-
-      <Separator />
-
-      <SchedulesDay>
-        <Text
-          as="h3"
-          size={16}
-          className={inter.className}
-          fontVariant="small-caps"
-        >
-          QUI
-        </Text>
-
-        <Text as="h3" size={16} $weight="500" className={inter.className}>
-          {qui.fechado
-            ? 'FECHADO'
-            : `${formatHour(qui.inicio)} - ${formatHour(qui.termino)}`}
-        </Text>
-      </SchedulesDay>
-
-      <Separator />
-
-      <SchedulesDay>
-        <Text
-          as="h3"
-          size={16}
-          className={inter.className}
-          fontVariant="small-caps"
-        >
-          SEX
-        </Text>
-
-        <Text as="h3" size={16} $weight="500" className={inter.className}>
-          {sex.fechado
-            ? 'FECHADO'
-            : `${formatHour(sex.inicio)} - ${formatHour(sex.termino)}`}
-        </Text>
-      </SchedulesDay>
-
-      <Separator />
-
-      <SchedulesDay>
-        <Text
-          as="h3"
-          size={16}
-          className={inter.className}
-          fontVariant="small-caps"
-        >
-          SAB
-        </Text>
-
-        <Text as="h3" size={16} $weight="500" className={inter.className}>
-          {sab.fechado
-            ? 'FECHADO'
-            : `${formatHour(sab.inicio)} - ${formatHour(sab.termino)}`}
-        </Text>
-      </SchedulesDay>
-
-      <Separator />
-
-      <SchedulesDay>
-        <Text
-          as="h3"
-          size={16}
-          className={inter.className}
-          fontVariant="small-caps"
-        >
-          DOM
-        </Text>
-
-        <Text as="h3" size={16} $weight="500" className={inter.className}>
-          {dom.fechado
-            ? 'FECHADO'
-            : `${formatHour(dom.inicio)} - ${formatHour(dom.termino)}`}
-        </Text>
-      </SchedulesDay>
+          {day.name !== 'dom' && <Separator />}
+        </Fragment>
+      ))}
     </SchedulesDaysComponent>
   )
 }
