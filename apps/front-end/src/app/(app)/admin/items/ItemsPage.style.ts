000c02afd839f1f54331d57791195888408364b2
Davi Alves <cadwvk@gmail.com>
2024-02-21 16:07:53 -0300

    feat: Add styling and functionality to ItemsPage

--- apps/front-end/src/app/(app)/admin/items/ItemsPage.style.ts
+++ apps/front-end/src/app/(app)/admin/items/ItemsPage.style.ts
@@ -10,4 +10,38 @@ export const ItemsPageRoot = styled.main`
 
   width: 100%;
   padding: 2rem;
+  gap: 1.75rem;
+`
+
+export const SeparatorCategories = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+
+  width: 100%;
+  height: fit-content;
+
+  .text {
+    color: ${({ theme }) => theme.border};
+  }
+
+  .line {
+    width: 100%;
+    height: 5px;
+    border-radius: 9px;
+
+    background-color: ${({ theme }) => theme.bodyTertiary};
+  }
+`
+
+export const CategoriesListRoot = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  flex-direction: column;
+
+  gap: 0.75rem;
+
+  width: 100%;
 `
--- apps/front-end/src/app/(app)/admin/items/page.tsx
+++ apps/front-end/src/app/(app)/admin/items/page.tsx
@@ -1,10 +1,66 @@
-import { ItemsPageRoot } from './ItemsPage.style'
+'use client'
+
+import { Text } from '@/shared/components/text/Text'
+import {
+  CategoriesListRoot,
+  ItemsPageRoot,
+  SeparatorCategories,
+} from './ItemsPage.style'
 import { AddCategory } from './utils/components/add-category/AddCategory'
+import { useGetCategories } from '@/shared/hooks/useGetCategories'
+import { Skeleton } from '@/shared/components/skeleton/Skeleton'
+import { CategoryItem } from './utils/components/category-item'
+import { Separator } from '@/shared/components/separator/Separator.style'
+import { ExternalLink, Trash2 } from 'lucide-react'
+
+const skeletonArray = Array.from({ length: 10 })
 
 export default function ItemsPage() {
+  const categories = useGetCategories()
+
+  const categoriesData = categories.categories
+  const loadingCategories = categories.loading
+
   return (
     <ItemsPageRoot>
       <AddCategory />
+
+      <SeparatorCategories>
+        <span className="line" />
+        <Text size={18} $weight="700" className="text">
+          Categorias
+        </Text>
+        <span className="line" />
+      </SeparatorCategories>
+
+      <CategoriesListRoot>
+        {loadingCategories
+          ? skeletonArray.map((_, index) => (
+              <Skeleton
+                key={index}
+                width="100%"
+                height="3rem"
+                $borderRadius={0.375}
+              />
+            ))
+          : categoriesData.map((category) => (
+              <CategoryItem.Root key={category.id}>
+                <CategoryItem.Name>{category.name}</CategoryItem.Name>
+
+                <Separator direction="vertical" />
+
+                <CategoryItem.Actions>
+                  <CategoryItem.Button>
+                    <ExternalLink />
+                  </CategoryItem.Button>
+
+                  <CategoryItem.Button>
+                    <Trash2 />
+                  </CategoryItem.Button>
+                </CategoryItem.Actions>
+              </CategoryItem.Root>
+            ))}
+      </CategoriesListRoot>
     </ItemsPageRoot>
   )
 }
