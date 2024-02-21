7c77ae0502b79ef8486d5e0cd597b19c989192fe
Davi Alves <cadwvk@gmail.com>
2024-02-21 20:59:20 -0300

    feat: Add CategoryPage component and styles

---
+++ apps/front-end/src/app/(app)/admin/items/categorias/[id]/CategoryPage.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const CategoryPageRoot = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  flex-direction: column;
+
+  width: 100%;
+  padding: 2rem;
+  gap: 1rem;
+`
--- apps/front-end/src/app/(app)/admin/items/categorias/[id]/page.tsx
+++ apps/front-end/src/app/(app)/admin/items/categorias/[id]/page.tsx
@@ -1,3 +1,39 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+import { CategoryPageRoot } from './CategoryPage.style'
+import { useContext } from 'react'
+import { CategoriesContext } from '@/shared/contexts/Categories'
+import { SeparatorWithName } from '@/shared/components/separator/SeparatorWithName'
+import { Toolbar } from './utils/components/toolbar'
+import { Plus } from 'lucide-react'
+import { Text } from '@/shared/components/text/Text'
+import { useTheme } from 'styled-components'
+
 export default function CategoryPage() {
-  return <div>CategoryPage</div>
+  const theme = useTheme()
+  const { id } = useParams()
+  const { categories } = useContext(CategoriesContext)
+
+  const category = categories.find((category) => category.id === id)
+  const categoryName = category?.name || 'Carregando...'
+
+  return (
+    <CategoryPageRoot>
+      <Toolbar.Root>
+        <Toolbar.Title>{categoryName}</Toolbar.Title>
+
+        <Toolbar.Actions>
+          <Toolbar.Button>
+            <Plus color={theme.iconSecondary} />
+            <Text size={16} $weight="600">
+              Criar produto
+            </Text>
+          </Toolbar.Button>
+        </Toolbar.Actions>
+      </Toolbar.Root>
+
+      <SeparatorWithName name="Produtos" />
+    </CategoryPageRoot>
+  )
 }
