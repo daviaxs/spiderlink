cd640747b5565852884abd24d48bdaea3651c88b
Davi Alves <cadwvk@gmail.com>
2024-02-21 21:08:34 -0300

    Update: toolbar title font size

--- apps/front-end/src/app/(app)/admin/items/categorias/[id]/utils/components/toolbar/ToolbarTitle.tsx
+++ apps/front-end/src/app/(app)/admin/items/categorias/[id]/utils/components/toolbar/ToolbarTitle.tsx
@@ -2,7 +2,7 @@ import { Text } from '@/shared/components/text/Text'
 
 export function ToolbarTitle({ children }: { children: React.ReactNode }) {
   return (
-    <Text size={18} $weight="600">
+    <Text size={24} $weight="600">
       {children}
     </Text>
   )
