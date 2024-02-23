53047517be0e54513065f2cafe02fb3f098e0cb2
Davi Alves <cadwvk@gmail.com>
2024-02-23 16:04:54 -0300

    setup: Add remotePatterns for Cloudinary images

--- apps/front-end/next.config.mjs
+++ apps/front-end/next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
