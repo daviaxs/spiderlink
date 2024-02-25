c0902b1a6c255c2605e14c010ae0ce9528dba33b
Davi Alves <cadwvk@gmail.com>
2024-02-25 14:05:13 -0300

    Add metadata to layout component

--- apps/front-end/src/app/layout.tsx
+++ apps/front-end/src/app/layout.tsx
@@ -1,8 +1,10 @@
 import { ECOMMERCE_NAME } from '@/shared/constants/names'
+import { Metadata } from 'next'
 import { ReactNode } from 'react'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: ECOMMERCE_NAME,
+  description: ECOMMERCE_NAME,
 }
 
 export default function AppLayout({ children }: { children: ReactNode }) {
