0f9c7f75a1927d9360e37dc547f675c2a66c0d38
Davi Alves <cadwvk@gmail.com>
2024-02-27 17:31:35 -0300

    Refactor: Products component and add skeleton loading

--- apps/front-end/src/shared/components/products/Products.tsx
+++ apps/front-end/src/shared/components/products/Products.tsx
@@ -1,19 +1,56 @@
+'use client'
+
+import { convertPriceToBRFormat } from '@/shared/functions/convertPriceToBRFormat'
 import { ProductsRoot } from './Products.style'
-import { ProductItemRoot } from './utils/components/ProductItem/ProductItemRoot'
+import { ProductItem } from './utils/components/ProductItem/ProductItem'
+import { useContext, useEffect } from 'react'
+import { ProductsContext } from '@/shared/contexts/Products'
+import { useSearchParams } from 'next/navigation'
+import { ProductsSkeletonRoot } from './utils/ProductsSkeleton.style'
+import { Skeleton } from '../skeleton/Skeleton'
+
+const skeletons = Array.from({ length: 4 })
 
 export function Products() {
-  return (
+  const { products, setCategoryId } = useContext(ProductsContext)
+  const searchParams = useSearchParams()
+
+  const categoryId = searchParams.get('category')
+
+  useEffect(() => {
+    if (categoryId) {
+      setCategoryId(categoryId)
+    }
+  }, [categoryId, setCategoryId])
+
+  return products.length > 0 ? (
     <ProductsRoot>
-      <ProductItemRoot />
-      <ProductItemRoot />
-      <ProductItemRoot />
-      <ProductItemRoot />
-      <ProductItemRoot />
-      <ProductItemRoot />
-      <ProductItemRoot />
-      <ProductItemRoot />
-      <ProductItemRoot />
-      <ProductItemRoot />
+      {products.map((product) => (
+        <ProductItem.Root key={product.id}>
+          <ProductItem.Infos>
+            <ProductItem.Title>{product.name}</ProductItem.Title>
+            <ProductItem.Description>
+              {product.description}
+            </ProductItem.Description>
+            <ProductItem.Price>
+              {convertPriceToBRFormat(product.price as number)}
+            </ProductItem.Price>
+          </ProductItem.Infos>
+
+          <ProductItem.Image src={product.img} />
+        </ProductItem.Root>
+      ))}
     </ProductsRoot>
+  ) : (
+    <ProductsSkeletonRoot>
+      {skeletons.map((_, index) => (
+        <Skeleton
+          key={index}
+          height="11rem"
+          width="100%"
+          $borderRadius={0.375}
+        />
+      ))}
+    </ProductsSkeletonRoot>
   )
 }
---
+++ apps/front-end/src/shared/components/products/utils/ProductsSkeleton.style.ts
@@ -0,0 +1,19 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const ProductsSkeletonRoot = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+
+  width: 100%;
+  height: fit-content;
+
+  padding: 2rem;
+  gap: 2rem;
+
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(1, 1fr);
+    gap: 0;
+  }
+`
