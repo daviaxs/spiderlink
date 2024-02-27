de97bbda6c262469a811e231248f6e72a7da57aa
Davi Alves <cadwvk@gmail.com>
2024-02-27 17:39:17 -0300

    feat: Add skeleton loading for empty categories

--- apps/front-end/src/shared/components/categorys/utils/CategoryList.tsx
+++ apps/front-end/src/shared/components/categorys/utils/CategoryList.tsx
@@ -8,6 +8,9 @@ import {
 } from '../Categorys.style'
 import { CategoryItem } from './CategoryItem'
 import { CategoriesContext } from '@/shared/contexts/Categories'
+import { Skeleton } from '../../skeleton/Skeleton'
+
+const skeletons = Array.from({ length: 10 })
 
 export function CategoryList() {
   const { categories } = useContext(CategoriesContext)
@@ -16,16 +19,25 @@ export function CategoryList() {
   return (
     <CategoryListRoot>
       <CategoryScroll>
-        {categories.map((category) => (
-          <CategoryItem
-            key={category.id}
-            title={category.name}
-            id={category.id}
-            focusId={focusId}
-            setFocusId={setFocusId}
-            categories={categories}
-          />
-        ))}
+        {categories.length === 0
+          ? skeletons.map((_, index) => (
+              <Skeleton
+                key={index}
+                height="85px"
+                width="90px"
+                $borderRadius={0.375}
+              />
+            ))
+          : categories.map((category) => (
+              <CategoryItem
+                key={category.id}
+                title={category.name}
+                id={category.id}
+                focusId={focusId}
+                setFocusId={setFocusId}
+                categories={categories}
+              />
+            ))}
       </CategoryScroll>
 
       <CategoryShadow />
