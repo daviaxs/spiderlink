b4c51e942dbe95ea14003588b98f4a47c9e9b016
Davi Alves <cadwvk@gmail.com>
2024-02-29 18:30:32 -0300

    feat: Add CartContext and interfaces for cart functionality

---
+++ apps/front-end/src/shared/contexts/cart-context/interfaces.ts
@@ -0,0 +1,42 @@
+export interface OptionProps {
+  quantity: number
+  id: string
+  name: string
+  description: string
+  price: number
+}
+
+export interface SubsectionProps {
+  id: string
+  name: string
+  limit: number
+  required: boolean
+  multipleChoice: boolean
+  Options?: OptionProps[]
+}
+
+export interface ProductProps {
+  id: string
+  name: string
+  price: number | string
+  img: string
+  description: string
+  Subsection?: SubsectionProps[]
+}
+
+export interface CartState {
+  products: ProductProps[]
+  productQuantity: { [key: string]: number }
+  optionQuantity: { [key: string]: number }
+}
+
+export interface CartActions {
+  addProduct: (product: ProductProps, subsections: SubsectionProps[]) => void
+  removeProduct: (productId: string) => void
+  addProductQuantity: (productId: string) => void
+  removeProductQuantity: (productId: string) => void
+  addOptionQuantity: (productId: string, optionId: string) => void
+  removeOptionQuantity: (productId: string, optionId: string) => void
+  removeOption: (productId: string, optionId: string) => void
+  clearCart: () => void
+}
