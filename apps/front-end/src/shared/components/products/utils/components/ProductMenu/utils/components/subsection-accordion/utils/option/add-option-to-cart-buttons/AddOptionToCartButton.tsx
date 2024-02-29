1639a4fe457b754d4737d87bcef96b0f4c6a3238
Davi Alves <cadwvk@gmail.com>
2024-02-28 21:02:28 -0300

    feat: Add disabled prop to AddOptionToCartButton component

--- apps/front-end/src/shared/components/products/utils/components/ProductMenu/utils/components/subsection-accordion/utils/option/add-option-to-cart-buttons/AddOptionToCartButton.tsx
+++ apps/front-end/src/shared/components/products/utils/components/ProductMenu/utils/components/subsection-accordion/utils/option/add-option-to-cart-buttons/AddOptionToCartButton.tsx
@@ -3,14 +3,20 @@ import { AddOptionToCartButtonStyle } from './AddOptionToCartButton.style'
 
 interface AddOptionToCartButtonProps extends HTMLAttributes<HTMLButtonElement> {
   children: ReactNode
+  disabled?: boolean
 }
 
 export function AddOptionToCartButton({
   children,
+  disabled,
   ...props
 }: AddOptionToCartButtonProps) {
   return (
-    <AddOptionToCartButtonStyle className="action-button" {...props}>
+    <AddOptionToCartButtonStyle
+      disabled={disabled}
+      className="action-button"
+      {...props}
+    >
       {children}
     </AddOptionToCartButtonStyle>
   )
