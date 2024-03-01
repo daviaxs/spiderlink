350d6078f95ddb2bc1fd2a207b64b6672ee693f7
Davi Alves <cadwvk@gmail.com>
2024-03-01 20:35:15 -0300

    feat: Add link to home page in header

--- apps/front-end/src/shared/components/header/Header.tsx
+++ apps/front-end/src/shared/components/header/Header.tsx
@@ -3,6 +3,8 @@ import { Text } from '../text/Text'
 import { Menu } from '../menu/Menu'
 import { useShowHeader } from '@/shared/hooks/useShowHeader'
 import { CartMenu } from '../cart-menu/CartMenu'
+import { ECOMMERCE_NAME } from '@/shared/constants/names'
+import Link from 'next/link'
 
 export function Header({ toggleTheme }: { toggleTheme?: () => void }) {
   const showHeader = useShowHeader()
@@ -12,9 +14,19 @@ export function Header({ toggleTheme }: { toggleTheme?: () => void }) {
       <div className="header-info">
         <Menu toggleTheme={toggleTheme} />
 
-        <Text as="h2" size={30} fontVariant="all-small-caps" $weight="900">
-          spiderlink
-        </Text>
+        <Link
+          href={'/'}
+          style={{
+            padding: 0,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Text as="h2" size={30} fontVariant="all-small-caps" $weight="900">
+            {ECOMMERCE_NAME}
+          </Text>
+        </Link>
       </div>
 
       <CartMenu />
