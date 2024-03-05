cb59163be75ad0dab606e8656bb2d78fb9b3e7cb
Spiderlink-org <spiderlink.sites@gmail.com>
2024-03-05 14:08:38 -0300

    Update initial width value in useWindowWidth hook

--- apps/front-end/src/shared/hooks/useWindowWidth.ts
+++ apps/front-end/src/shared/hooks/useWindowWidth.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
 export const useWindowWidth = () => {
-  const [width, setWidth] = useState(0)
+  const [width, setWidth] = useState(700)
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
