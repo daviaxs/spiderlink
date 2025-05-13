375e1f1905b7a03618198ef07c07ab8d1cfa7096
Davi Alves <109174765+daviaxs@users.noreply.github.com>
2025-05-13 20:56:26 -0300

    Update domains.ts

--- apps/back-end/src/domains.ts
+++ apps/back-end/src/domains.ts
@@ -1,1 +1,1 @@
-export const domains = ['http://localhost:3000', 'https://www.spider-link.pro']
+export const domains = ['http://localhost:3000', 'https://www.spider-link.pro', 'https://spiderlink.vercel.app']
